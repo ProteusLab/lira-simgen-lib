a928d51c856f621bed075b0767e5402eb739a7a4
uslstenn <shurygin0171@gmail.com>
2026-09-16 15:40:54 +0300

    [interpreter] Add RISC-V specific syscall handling

---
+++ interpreter/runtime/Target/RISC_V/cpu_state_ext.cc
@@ -0,0 +1,17 @@
+#include "cpu_state.hh"
+#include <cstdint>
+
+namespace prot::state {
+
+void CPU::sysCall() {
+  switch (const auto syscallNum = getXRegs<isa::Word>(17)) {
+  case 93:
+    doExit(getXRegs<isa::Word>(10));
+    break;
+  default:
+    throw std::runtime_error{
+        fmt::format("Unknown syscall w/ num {}", syscallNum)};
+  }
+}
+
+} // namespace prot::state
